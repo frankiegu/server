3de866b5b9a6e25f402977d50d522115925395c1
Nirmal Almara <hello@nirm.al>
2018-08-22 14:06:14 +0530

    Sign in - add logic to check if onboard is complete

--- src/onboard/OnboardSignIn.js
+++ src/onboard/OnboardSignIn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Subscribe } from 'unstated';
 import { Redirect } from "react-router-dom";
 import OnboardContainer from '../containers/OnboardContainer';
+import OnboardRoute from './OnboardRoute';
 
 function OnboardSignIn(props) {
   return (
@@ -11,12 +12,16 @@ function OnboardSignIn(props) {
         ?
           <Redirect to="/" />
         :
-          <form className="onboard">
-            <label className="onboard__label">Sign In</label>
-            <input type="text" className="onboard__usernameoremail" id="signInUsernameOrEmail" placeholder="Username / Email address" />
-            <input type="password" className="onboard__password" id="signInPassword" placeholder="Password" />
-            <input type="submit" className="button button--primary onboard__submit" value="Submit" onClick={(event) => onboard.signIn(event, props.signInAPI)} />
-          </form>
+          onboard.state.isStorageFilled
+          ?
+            <form className="onboard">
+              <label className="onboard__label">Sign In</label>
+              <input type="text" className="onboard__usernameoremail" id="signInUsernameOrEmail" placeholder="Username / Email address" />
+              <input type="password" className="onboard__password" id="signInPassword" placeholder="Password" />
+              <input type="submit" className="button button--primary onboard__submit" value="Submit" onClick={(event) => onboard.signIn(event, props.signInAPI)} />
+            </form>
+          :
+            <OnboardRoute />
       )}
     </Subscribe>
   );
